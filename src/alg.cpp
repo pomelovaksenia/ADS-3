6221856ba3a7f540b83f6ebbb569bffbde3a59a3
pomelovaksenia <116708935+pomelovaksenia@users.noreply.github.com>
2023-03-19 22:01:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int lefty = 0; 
+  int righty = size - 1; 
+  int county = 0; 
+  int middly = 0; 
+  while (righty >= lefty) { 
+    middly = (righty + lefty) / 2; 
+    if (arr[middly] == value) { 
+      county++; 
+      int sered = middly - 1; 
+      while (sered >= lefty && arr[sered] == value) { 
+        county++; 
+        sered--; 
+      } 
+      ser = middly + 1; 
+      while (sered <= righty && arr[sered] == value) { 
+        county++; 
+        sered++; 
+      } 
+      break; 
+    } else if (value < arr[middly]) { 
+      righty = middly - 1; 
+    } else { 
+      lefty = middly + 1; 
+    } 
+  } 
+  return county; 
 }
