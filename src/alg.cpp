deb2039345ff5d0621217963789f63bc39bd14bf
pomelovaksenia <116708935+pomelovaksenia@users.noreply.github.com>
2023-03-28 21:45:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int right = size - 1;
+    int left = 0;
+    int counter = 0;
+    while (left <= right) {
+        int middle = (left + right) / 2;
+        if (*(arr + middle) == value) {
+            counter++;
+            int newMiddle = middle;
+            while ((*(arr + middle + 1)) == value) {
+                middle++;
+                counter++;                
+            }
+            while ((*(arr + newMiddle - 1)) == value) {
+                newMiddle--;
+                counter++; 
+            }
+            return counter;
+        } else if (*(arr + middle) > value) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+    return counter;
 }
