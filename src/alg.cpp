1adb014135383570d18005e2f29b4c4fda7800a4
pomelovaksenia <116708935+pomelovaksenia@users.noreply.github.com>
2023-03-28 21:55:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,30 +1,28 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  int lefty = 0; 
-  int righty = size - 1; 
-  int county = 0; 
-  int middly = 0; 
-  while (righty >= lefty) { 
-    middly = (righty + lefty) / 2; 
-    if (arr[middly] == value) { 
-      county++; 
-      int sered = middly - 1; 
-      while (sered >= lefty && arr[sered] == value) { 
-        county++; 
-        sered--; 
-      } 
-      ser = middly + 1; 
-      while (sered <= righty && arr[sered] == value) { 
-        county++; 
-        sered++; 
-      } 
-      break; 
-    } else if (value < arr[middly]) { 
-      righty = middly - 1; 
-    } else { 
-      lefty = middly + 1; 
-    } 
-  } 
-  return county; 
+int cbinsearch(int* arr, int size, int value) {
+    int counter = 0;
+    int right = size - 1;
+    int left = 0;
+    while (left <= right) {
+        int middle = (right + left) / 2;
+        if (*(arr + middle) == value) {
+            counter++;
+            int newmiddle = middle;
+            while ((*(arr + middle + 1)) == value) {
+                middle++;
+                counter++;
+            }
+            while ((*(arr + newmiddle - 1)) == value) {
+                newmiddle--;
+                counter++;
+            }
+            return counter;
+        } else if (*(arr + middle) > value) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+    return counter;
 }
